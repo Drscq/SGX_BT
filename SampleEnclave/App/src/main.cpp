c086b8c0a66fcdba92fc6c2eb7d6b834a3767131
changqi <84911039+comeonsunny@users.noreply.github.com>
2025-04-02 02:57:35 +0000

    Just use the 1 dummy blocks.

--- SampleEnclave/App/src/main.cpp
+++ SampleEnclave/App/src/main.cpp
@@ -146,7 +146,7 @@ int main(int argc, char* argv[]) {
         client.InitCreateBinaryTree();
         
         // Randomly access the blocks
-        size_t accessCount = 2;
+        size_t accessCount = 100;
         for (size_t i = 0; i < accessCount; ++i) {
             std::cout << "Iteration " << i << std::endl;
             // DEBUG_PRINT("Iteration " << i);
