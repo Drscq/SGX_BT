1fe5ba3ae0375170a15e6ee04d35fde60f1313b0
comeonsunny <changqi@vt.edu>
2025-04-04 17:19:10 -0400

    Update the config.h to create the logs files.

--- experimentalResults/src/config.py
+++ experimentalResults/src/config.py
@@ -3,7 +3,7 @@ dir_layer_1 = ['readPath', "earlyReshuffle", "evictPath"]
 dir_layer_2 = ['breakDownCost']
 dir_layer_3 = ['64KB', "256KB", "1024KB", 'blockSize']
 dir_layer_4 = ['client', 'server']
-dir_layer_5 = ['bin']
+dir_layer_5 = ['bin', 'logs']
 
 
 def clean_directory_with_exception(
